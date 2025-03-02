1212189f0a28bb1c0f0382563175e742ce3d985f
Kavindya19 <kavijojo952@gmail.com>
2025-03-02 20:12:02 +0530

    Add files via upload

---
+++ CT_2021_007/LW01/src/Q_04/Q_04.java
@@ -0,0 +1,21 @@
+package Q_04;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+public class Q_04 {
+    public static void main(String[] args) {
+
+        /*LocalDate today = LocalDate.now();
+        System.out.println(today);*/
+
+        Date day = new Date();
+        System.out.println(day);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
+        System.out.println(sdf.format(day));
+
+    }
+}
